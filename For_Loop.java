f6b978e1f5f36dbd229750dc4035cd48b265a567
Swapnil-Ghodekar <129079172+Swapnil-Ghodekar@users.noreply.github.com>
2023-03-28 00:49:34 +0530

    For loop code in java

---
+++ For_Loop.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class For_Loop {
+
+	public static void main(String[] args)
+	{
+		//look for loop 
+		for(int i=1; i<=10; i++)
+		{
+			System.out.println("Welcome");            // print welcomw 10 times
+		}
+		
+	}
+
+}
