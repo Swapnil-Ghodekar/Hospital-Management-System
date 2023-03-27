78d94d3eb51ccc39efa3a3eff9c94a1a99b14e69
Swapnil-Ghodekar <129079172+Swapnil-Ghodekar@users.noreply.github.com>
2023-03-28 03:17:22 +0530

    Add files via upload
    This is java code to take input from user

---
+++ Input From User.java
@@ -0,0 +1,27 @@
+package day_3;
+
+
+import java.util.Scanner;          // import package from java from scanner
+public class Input {
+
+	public static void main(String[] args)
+	
+	{
+		//take input as a integer from a user
+		Scanner sc = new Scanner(System.in);    // crteate object of scanner class 
+		
+		System.out.println("Enter number:");     // print msg for user
+		int a = sc.nextInt();                       // call a predefined method ta take integer input
+		
+		System.out.println("Your Enter number is:");
+		System.out.println(a);             // print value under a
+		
+		String name = sc.next();         // for string
+		float f = sc.nextFloat();           // for float value
+		long l = sc.nextLong();                // for long integer
+		char ch = sc.
+				
+
+	}
+
+}
