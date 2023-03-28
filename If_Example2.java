ce15b241fb44df235171d5ffb3176ebdfa3e252c
Swapnil-Ghodekar <129079172+Swapnil-Ghodekar@users.noreply.github.com>
2023-03-28 16:59:45 +0530

    Add files via upload
    Upload for merge operation done

---
+++ If_Example2.java
@@ -0,0 +1,23 @@
+package day_2;
+
+public class If_Example2 {
+
+	public static void main(String[] args)
+	{
+		
+	                  
+		if(10>=2)      // condition inside a if block is only true or false, it will take only boolean 
+		{
+			System.out.println("Welcome");
+			
+		}
+		else
+		{
+			System.out.println("Sorry");
+		}
+		
+		// if(false)
+		// if(true)          // on this principle if else work, take boolean value for condition check         
+	}
+
+}
